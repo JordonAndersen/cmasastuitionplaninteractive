7dc612c485c4719c5b288d0c350ea65bfa992096
jordonandersen <jordonandsersen9@gmail.com>
2025-03-21 08:19:23 -0600

    initial, html,css,js first run

---
+++ toggle.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Page has fully loaded.")
+})
+
+
+function elemPlan(){
+    console.log("elementary is loaded")
+    // updating personalized card 
+    document.getElementById("personalizedicon").src = "img/Elementary icon 1.png"
+    document.getElementById("personalizedprice").innerHTML = "$2,750 Tuition"
+     // updating essential card 
+     document.getElementById( "essentialicon" ).src = "img/Elementary icon 3.png"
+     document.getElementById("essentialprice").innerHTML = "$6,660 Tuition"
+     // updating premier card 
+     document.getElementById("premiericon" ).src = "img/Elementary icon 2.png"
+     document.getElementById("premierprice").innerHTML = "$7,750 Tuition"
+    
+
+}
+
+function msPlan(){
+    console.log("Middle School is loaded")
+    // updating personalized card 
+    document.getElementById("personalizedicon").src = "img/Middle School Icon 1.png"
+    document.getElementById("personalizedprice").innerHTML = "$2,750 Tuition"
+     // updating essential card 
+    document.getElementById("essentialicon" ).src = "img/Middle School Icon 2.png"
+    document.getElementById("essentialprice").innerHTML = "$7,500 Tuition"
+     // updating premier card 
+    document.getElementById("premiericon" ).src = "img/Middle School Icon 3.png"
+    document.getElementById("premierprice").innerHTML = "$8,500 Tuition"
+
+}
+
+function hsPlan(){
+    console.log("High School is loaded")
+    // updating personalized card 
+    document.getElementById("personalizedicon", "essentialicon", "premiericon" ).src = "img/High School Icon.png"
+    document.getElementById("personalizedprice").innerHTML = "$2,750 Tuition"
+    // updating essential card 
+    document.getElementById( "essentialicon" ).src = "img/High School Icon.png"
+    document.getElementById("essentialprice").innerHTML = "$2,750 Tuition"
+    // updating premier card 
+    document.getElementById("premiericon" ).src = "img/High School Icon.png"
+    document.getElementById("premierprice").innerHTML = "$2,750 Tuition"
+
+}
